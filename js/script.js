0d29369a1062e72ced5f5131ea24a74484042ee6
NPosse <nicolasm.posse@gmail.com>
2026-06-03 15:23:18 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,41 +1,79 @@
 console.log("HD INSUMOS listo 🚀");
 
-// ANIMACION SUAVE SCROLL
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-anchor.addEventListener('click', function (e) {
-    e.preventDefault();
-
-    document.querySelector(this.getAttribute('href')).scrollIntoView({
-    behavior: 'smooth'
-    });
-});
-});
-
 const menu =
 document.querySelector(".menu-toggle");
 
 const links =
 document.querySelector(".nav-links");
 
+const navLinks =
+document.querySelectorAll(
+'.nav-links a'
+);
+
+
+// MENU
 
 menu.addEventListener(
-"click",
+'click',
 
 ()=>{
 
 links.classList.toggle(
-"active"
+'active'
 );
 
 }
 );
 
-document.querySelectorAll('.nav-links a').forEach(link => {
 
-    link.addEventListener('click', () => {
+// SCROLL + CERRAR MENU
 
-        links.classList.remove('active');
+navLinks.forEach(
 
-    });
+(link)=>{
+
+link.addEventListener(
+
+'click',
+
+(e)=>{
+
+e.preventDefault();
+
+const target =
+document.querySelector(
+
+link.getAttribute(
+'href'
+)
+
+);
+
+
+// cerrar menu
+
+links.classList.remove(
+'active'
+);
+
+
+// esperar animacion
+
+setTimeout(()=>{
+
+target.scrollIntoView({
+
+behavior:'smooth'
 
 });
+
+},200);
+
+}
+
+);
+
+}
+
+);
